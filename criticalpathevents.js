9ded85d4c12f0a8208483c71839a2beb661f6484
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-03-25 13:36:59 +0100

    critical render path

---
+++ criticalpathevents.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+});
+window.addEventListener("load", function (event) {
+  console.log("All resources finished loading!");
+});
