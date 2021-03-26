707f161737a49a9a2b075749a992b2a70b23260a
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-03-26 12:55:59 +0100

    algo fib

---
+++ algo/fibCache.js
@@ -0,0 +1,23 @@
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
