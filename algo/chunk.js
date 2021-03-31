12a5a6ba7b302d870fbeea1a05ef8a4b9d914a12
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-03-31 12:59:35 +0100

    chunk algo

---
+++ algo/chunk.js
@@ -0,0 +1,11 @@
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
